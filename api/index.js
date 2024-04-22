4e6df79c56b1145737f3648a19f21591a82d2e55
Molina-Q <quentinmolina1@gmail.com>
2024-04-22 15:41:07 +0200

    added authRoutes

--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import userRoutes from './routes/user.route.js'
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 
@@ -16,8 +17,10 @@ mongoose
 
 const app = express();
 
-app.use('/api/user', userRoutes);
+app.use(express.json());
 
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
 
 app.listen(3000, () => {
 	console.log("Server is running on port 3000");
