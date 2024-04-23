7633e3038f82abda2915f5e63eb8bc516b43ca94
Molina-Q <quentinmolina1@gmail.com>
2024-04-23 09:12:37 +0200

    added footer inside browserRouter

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,18 +7,21 @@ import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Dashboard from "./pages/Dashboard";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 export default function App() {
 	return (
-  <BrowserRouter>
-  <Header />
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/about' element={<About />} />
-      <Route path='/sign-in' element={<SignIn />} />
-      <Route path='/sign-up' element={<SignUp />} />
-      <Route path='/projects' element={<Projects />} />
-      <Route path='/dashboard' element={<Dashboard />} />
-    </Routes>
-  </BrowserRouter>
-)}
+		<BrowserRouter>
+			<Header />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/about" element={<About />} />
+				<Route path="/sign-in" element={<SignIn />} />
+				<Route path="/sign-up" element={<SignUp />} />
+				<Route path="/projects" element={<Projects />} />
+				<Route path="/dashboard" element={<Dashboard />} />
+			</Routes>
+			<Footer />
+		</BrowserRouter>
+	);
+}
