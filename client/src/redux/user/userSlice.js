e6eef394201e93e0de08826e3151e12d64144a2a
Molina-Q <quentinmolina1@gmail.com>
2024-04-25 16:13:46 +0200

    added updateStart/success/failure reducer

--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -23,11 +23,24 @@ export const userSlice = createSlice({
       state.loading = false;
       state.error = action.payload;
     },
+    updateStart: (state) => {
+      state.loading = true;
+      state.error = null;
+    },
+    updateSuccess: (state, action) => {
+      state.currentUser = action.payload;
+      state.loading = false;
+      state.error = null;
+    },
+    updateFailure: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
 
   },
 })
 
 // Action creators are generated for each case reducer function
-export const { signInStart, signInFailure, signInSuccess } = userSlice.actions
+export const { signInStart, signInFailure, signInSuccess, updateStart, updateSuccess, updateFailure } = userSlice.actions
 
 export default userSlice.reducer
