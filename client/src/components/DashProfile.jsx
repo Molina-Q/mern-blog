8b6f76ec74572426f7668db57349243c52be3a5a
Molina-Q <quentinmolina1@gmail.com>
2024-04-25 17:19:26 +0200

    added signout functionnality

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -18,6 +18,7 @@ import {
 	deleteUserFailure,
 	deleteUserSuccess,
 	deleteUserStart,
+	signoutSuccess
 } from "../redux/user/userSlice";
 import { HiOutlineExclamationCircle } from "react-icons/hi";
 
@@ -139,27 +140,42 @@ export default function DashProfile() {
 
 	const handleDeleteUser = async () => {
 		setShowModal(false);
-	
+
 		try {
 			dispatch(deleteUserStart());
-			
+
 			const res = await fetch(`/api/user/delete/${currentUser._id}`, {
-				method: 'DELETE',
+				method: "DELETE",
 			});
 
 			const data = await res.json();
-			if(!res.ok) {
+			if (!res.ok) {
 				dispatch(deleteUserFailure(data.message));
-
 			} else {
 				dispatch(deleteUserStart(data));
-
 			}
 
 			dispatch(deleteUserSuccess());
 		} catch (error) {
 			dispatch(deleteUserFailure(error.message));
+		}
+	};
 
+	const handleSignout = async () => {
+		try {
+			const res = await fetch("/api/user/signout", {
+				method: "POST",
+			});
+			
+			const data = await res.json();
+
+			if(!res.ok) {
+				console.log(data.message);
+			} else {
+				dispatch(signoutSuccess());
+			}
+		} catch (error) {
+			console.log(error.message);
 		}
 	};
 
@@ -242,7 +258,9 @@ export default function DashProfile() {
 				<span onClick={() => setShowModal(true)} className="cursor-pointer">
 					Delete Account
 				</span>
-				<span className="cursor-pointer">Sign Out</span>
+				<span onClick={handleSignout} className="cursor-pointer">
+					Sign Out
+				</span>
 			</div>
 			{updateUserSuccess && (
 				<Alert color="success" className="mt-5">
@@ -255,7 +273,7 @@ export default function DashProfile() {
 					{updateUserError}
 				</Alert>
 			)}
-			
+
 			{error && (
 				<Alert color="failure" className="mt-5">
 					{error}
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,12 +4,32 @@ import { AiOutlineSearch } from "react-icons/ai";
 import { FaMoon, FaSun } from "react-icons/fa";
 import { useSelector, useDispatch } from "react-redux";
 import { toggleTheme } from "../redux/theme/themeSlice";
+import { signoutSuccess } from "../redux/user/userSlice";
 
 export default function Header() {
 	const path = useLocation().pathname;
 	const dispatch = useDispatch();
 	const { currentUser } = useSelector((state) => state.user);
 	const { theme } = useSelector((state) => state.theme);
+
+	const handleSignout = async () => {
+		try {
+			const res = await fetch("/api/user/signout", {
+				method: "POST",
+			});
+
+			const data = await res.json();
+
+			if (!res.ok) {
+				console.log(data.message);
+			} else {
+				dispatch(signoutSuccess());
+			}
+		} catch (error) {
+			console.log(error.message);
+		}
+	};
+
 	return (
 		<Navbar className="border-b-2">
 			<Link
@@ -66,8 +86,8 @@ export default function Header() {
 							<Dropdown.Item>Profile</Dropdown.Item>
 						</Link>
 						<Dropdown.Divider />
-						<Link to={"/sign-out"}>
-							<Dropdown.Item>Sign out</Dropdown.Item>
+						<Link to={"/"}>
+							<Dropdown.Item onClick={handleSignout}>Sign out</Dropdown.Item>
 						</Link>
 					</Dropdown>
 				) : (
