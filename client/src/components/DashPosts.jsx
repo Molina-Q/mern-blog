f42ede476dfcf44a3e720769e6fabf95d34e532e
Molina-Q <quentinmolina1@gmail.com>
2024-04-30 09:22:39 +0200

    added showMore functionnality

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -6,7 +6,8 @@ import { Link } from "react-router-dom";
 export default function DashPosts() {
 	const { currentUser } = useSelector((state) => state.user);
 	const [userPosts, setUserPosts] = useState([]);
-	console.log(userPosts);
+	const [showMore, setShowMore] = useState(true);
+
 	useEffect(() => {
 		const fetchPosts = async () => {
 			try {
@@ -15,6 +16,9 @@ export default function DashPosts() {
 
 				if (res.ok) {
 					setUserPosts(data.posts);
+					if (data.posts.length < 9) {
+						setShowMore(false);
+					}
 				}
 			} catch (error) {
 				console.log(error);
@@ -24,6 +28,24 @@ export default function DashPosts() {
 			fetchPosts();
 		}
 	}, [currentUser._id]);
+
+	const handleShowMore = async () => {
+		const startIndex = userPosts.length;
+		try {
+			const res = await fetch(
+				`/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`
+			);
+			const data = await res.json();
+			if(res.ok) {
+				setUserPosts((prev) => [...prev, ...data.posts]);
+				if(data.posts.length < 9 ) {
+					setShowMore(false);
+				}
+			}
+		} catch (error) {
+			console.log(error.message);
+		}
+	};
 
 	return (
 		<div className="table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500">
@@ -72,7 +94,9 @@ export default function DashPosts() {
 									<Table.Cell>{post.category}</Table.Cell>
 
 									<Table.Cell>
-										<span className="font-medium text-red-500 hover:underline cursor-pointer">Delete</span>
+										<span className="font-medium text-red-500 hover:underline cursor-pointer">
+											Delete
+										</span>
 									</Table.Cell>
 
 									<Table.Cell>
@@ -87,6 +111,14 @@ export default function DashPosts() {
 							))}
 						</Table.Body>
 					</Table>
+					{showMore && (
+						<button
+							onClick={handleShowMore}
+							className="w-full text-teal-500 self-center text-sm py-7"
+						>
+							Show more
+						</button>
+					)}
 				</>
 			) : (
 				<p>You have no posts yet.</p>
