b4aaac90471f881a9da0a6388cecc4ce66b7e02b
Molina-Q <quentinmolina1@gmail.com>
2024-05-02 17:00:45 +0200

    Added ScrollToTop

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,10 +13,12 @@ import OnlyAdminPrivateRoute from "./components/OnlyAdminPrivateRoute";
 import CreatePost from "./pages/CreatePost";
 import UpdatePost from "./pages/UpdatePost";
 import PostPage from "./pages/PostPage";
+import ScrollToTop from "./components/ScrollToTop";
 
 export default function App() {
 	return (
 		<BrowserRouter>
+			<ScrollToTop />
 			<Header />
 			<Routes>
 				<Route path="/" element={<Home />} />
@@ -32,7 +34,7 @@ export default function App() {
 					<Route path="/create-post" element={<CreatePost />} />
 					<Route path="/update-post/:postId" element={<UpdatePost />} />
 				</Route>
-			</Routes>
+			</Routes> 
 			<Footer />
 		</BrowserRouter>
 	);
